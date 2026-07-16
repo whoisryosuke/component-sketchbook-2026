61adaac092494f78afa00c0ce2f09fe601b31f90
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-07-16 16:35:49 -0700

    :wrench: no padding for preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -12,6 +12,8 @@ const preview: Preview = {
       },
     },
 
+    layout: 'fullscreen',
+
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
